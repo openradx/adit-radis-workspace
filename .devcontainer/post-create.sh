e2e5d4361a442a08e43964e397504f8a50284b3b
medihack <kai.schlamp@gmail.com>
2025-01-02 16:44:21 +0100

    Fix devcontainer setup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,40 +1,63 @@
 #!/bin/bash
 
-# The workspaces is by default owned by root.
-# We have to change this to clone additional repositories into /workspaces.
-sudo chown vscode:vscode /workspaces
-
-# Clone additional repositories
-git clone https://github.com/openradx/adit-radis-shared /workspaces/adit-radis-shared
-git clone https://github.com/openradx/adit /workspaces/adit
-git clone https://github.com/openradx/radis /workspaces/radis
-git clone https://github.com/openradx/adit-client /workspaces/adit-client
-git clone https://github.com/openradx/radis-client /workspaces/radis-client
-
-# Setup adit-radis-shared
-cd /workspaces/adit-radis-shared
-poetry install
-npm install
-poetry run invoke init-workspace
-
-# Setup adit
-cd /workspaces/adit
-poetry install
-poetry run invoke init-workspace
-
-# Setup radis
-cd /workspaces/radis
-poetry install
-poetry run invoke init-workspace
-
-# Setup adit-client
-cd /workspaces/adit-client
+# Install dependencies in adit-radis-workspace
 poetry install
 
-# Setup radis-client
-cd /workspaces/radis-client
-poetry install
+cd projects
 
-# Return to and setup adit-radis-workspace
-cd /workspaces/adit-radis-workspace
-poetry install
+if [ ! -d "adit-radis-shared" ]; then
+    echo "Setup 'adit-radis-shared'..."
+    git clone https://github.com/openradx/adit-radis-shared
+    cd adit-radis-shared
+    poetry install
+    npm install
+    poetry run invoke init-workspace
+    cd ..
+else
+    echo "'adit-radis-shared' already exists. Skipping."
+fi
+
+if [ ! -d "adit" ]; then
+    echo "Setup 'adit'..."
+    git clone https://github.com/openradx/adit
+    cd adit
+    poetry install
+    poetry run invoke init-workspace
+    cd ..
+else
+    echo "'adit' already exists. Skipping."
+fi
+
+if [ ! -d "radis" ]; then
+    echo "Setup 'radis'..."
+    git clone https://github.com/openradx/radis
+    cd radis
+    poetry install
+    poetry run invoke init-workspace
+    cd ..
+else
+    echo "'radis' already exists. Skipping."
+fi
+
+if [ ! -d "adit-client" ]; then
+    echo "Setup 'adit-client'..."
+    git clone https://github.com/openradx/adit-client
+    cd adit-client
+    poetry install
+    cd ..
+else
+    echo "'adit-client' already exists. Skipping."
+fi
+
+if [ ! -d "radis-client" ]; then
+    echo "Setup 'radis-client'..."
+    git clone https://github.com/openradx/radis-client
+    cd radis-client
+    poetry install
+    cd ..
+else
+    echo "'radis-client' already exists. Skipping."
+fi
+
+# Return to the workspace root
+cd ..
